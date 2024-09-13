7d21635f08c8351942f8cbd42bfe3c04abc9df15
elka <112241103+Loskutov2@users.noreply.github.com>
2024-09-13 18:01:22 +0300

    +

---
+++ src/HW-41-TypeScript.ts
@@ -0,0 +1,60 @@
+//1.
+
+// let age:number = 50;
+// let name:string = 'Max';
+// let toggle:boolean = true;
+// let empty:null = null;
+// let notInitialize:unknown = undefined;
+// let callback = (a:number) => { return 100 + a };
+
+//2.
+
+// let anything : any = -20;
+// anything = 'Text';
+// anything = {};
+
+//3.
+
+// let some:unknown;
+// some = 'Text';
+// let str: string;
+// str = some as string;
+
+//5.
+
+// let person:readonly [string, number] = ['Max', 21]
+
+//7.
+
+// let i : string|number
+
+//8.
+
+// let toggle: 'enable' | 'disable'
+
+//9.
+
+// function showMessage(message:string): void  {
+//     console.log(message);
+// }
+   
+// function calc(num1:number, num2:number): number {
+//     return num1 + num2;
+// }
+   
+// function customError() : never {
+//     throw new Error('Error');
+// }
+
+//10
+
+// type Page = {
+//     title: string,
+//     likes: number,
+//     accounts: string[],
+//     status: 'open'|'closed',
+//     details?: {
+//       createAt: Date,
+//       updateAt: Date,
+//     }
+// }
---
+++ src/main.ts
@@ -0,0 +1,25 @@
+// type PlayerObj = {
+//   name:string,
+//   game:string,
+//   lvl:number
+// }
+
+// const player:PlayerObj={
+//   name:'Nick',
+//   game:'DoodleJump',
+//   lvl:14200
+// }
+
+// function greetPlayer(playerData:PlayerObj){
+//   console.log('Hi', playerData.name)
+// }
+// greetPlayer(player)
+
+// type Alert = {
+//   state: 'error' | 'success' | 'warning',
+//   text: string
+// }
+// function notification(massage:Alert){
+//   alert(`${massage.state}:${massage.text}`)
+// }
+// notification({state:'success', text:'👌'})
